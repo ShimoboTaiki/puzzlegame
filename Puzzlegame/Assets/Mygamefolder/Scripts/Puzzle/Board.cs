23bb9205b3e1b19088ee4057395f995574b4de15
AtaruMatsudaira <matsu222212@gmail.com>
2022-03-29 17:32:50 +0900

    fix:ドロップが動かないバグを修正

--- Puzzlegame/Assets/Mygamefolder/Scripts/Puzzle/Board.cs
+++ Puzzlegame/Assets/Mygamefolder/Scripts/Puzzle/Board.cs
@@ -5,246 +5,212 @@ using Manager;
 using DG.Tweening;
 using UnityEngine.UI;
 using Cysharp.Threading.Tasks;
-using
-System;
+using System;
 using Random = UnityEngine.Random;
+using UniRx;
+using UniRx.Triggers;
 
 namespace Puzzle
 {
-    public class Board : MonoBehaviour
-    {
-        List<List<Drop>> dropList = new List<List<Drop>>();
-        public GameObject canvasObject;
-        public Text debugText;
-        
-        void Awake()
-        {
-            for (int i = 0; i < ParameterManager.Instance.boardSize.x; i++)
-            {
-                dropList.Add(new List<Drop>());
-                for (int j = 0; j < ParameterManager.Instance.boardSize.y; j++)
-                {
-                    Vector2Int pos = new Vector2Int(i, j);
-                    Type type = (Type)Random.Range(0, ColorManager.Instance.COLORMAXKIND);
-                    Drop drop = new Drop(pos, type,canvasObject);
-                    dropList[i].Add(drop);
-                }
-            }
-            Viewing();
-        }
+	public class Board : MonoBehaviour
+	{
+		List<List<Drop>> dropList = new List<List<Drop>>();
+		public GameObject canvasObject;
+		public Text debugText;
 
-        void Update()
-        {
-            if (dropList.Count == ParameterManager.Instance.boardSize.x)
-            {
-                string boardText = "";
-                for (int j =ParameterManager.Instance.boardSize.y-1; j >=0  ; j--)
-                {
-                    for (int i = 0; i < ParameterManager.Instance.boardSize.x; i++)
-                    {
-                        if (dropList[i][j] == null)
-                        {
-                            boardText += "N";
-                        }
-                        else
-                        {
-                            if (dropList[i][j].deleteFlag)
-                            {
-                                boardText += "<color=white>";
-                            }
-                            else
-                            {
-                                boardText += "<color=black>";
-                            }
-                            switch (dropList[i][j].type)
-                            {
-                                case Type.blue:
-                                    boardText += "B";
-                                    break;
-                                case Type.green:
-                                    boardText += "G";
-                                    break;
-                                case Type.yellow:
-                                    boardText += "Y";
-                                    break;
-                                case Type.red:
-                                    boardText += "R";
-                                    break;
-                            }
+		void Awake()
+		{
+			for (int i = 0; i < ParameterManager.Instance.boardSize.x; i++)
+			{
+				dropList.Add(new List<Drop>());
+				for (int j = 0; j < ParameterManager.Instance.boardSize.y; j++)
+				{
+					Vector2Int pos = new Vector2Int(i, j);
+					Type type = (Type) Random.Range(0, ColorManager.Instance.COLORMAXKIND);
+					Drop drop = new Drop(pos, type, canvasObject);
+					dropList[i].Add(drop);
+				}
+			}
 
-                            boardText += "</color>";
-                        }
-                    }
+			this.UpdateAsObservable().Subscribe(_ => Viewing()).AddTo(this);
+		}
 
-                    boardText += System.Environment.NewLine;
-                }
+		void Viewing()
+		{
+			string boardText = "";
+			for (int j = ParameterManager.Instance.boardSize.y - 1; j >= 0; j--)
+			{
+				for (int i = 0; i < ParameterManager.Instance.boardSize.x; i++)
+				{
+					if (dropList[i][j] == null)
+					{
+						boardText += "N";
+					}
+					else
+					{
+						if (dropList[i][j].deleteFlag)
+							boardText += "<color=white>";
+						else
+							boardText += "<color=black>";
+						boardText += dropList[i][j].type switch
+						{
+							Type.blue => "B",
+							Type.green => "G",
+							Type.yellow => "Y",
+							Type.red => "R",
+							_ => throw new ArgumentOutOfRangeException()
+						};
+						boardText += "</color>";
+					}
+				}
 
-                debugText.text = boardText;
-            }
-        }
+				boardText += System.Environment.NewLine;
+			}
 
-        void Viewing()
-        {
-            foreach(var drops in dropList)
-            {
-                foreach(var drop in drops)
-                {
-                    //GameObject dropobject = Instantiate(prefab,canvasObject.transform);
-                    //Vector2 pos = ((Vector2)drop.pos+new Vector2(-2,-2.5f))*100;
-                    //dropobject.transform.localPosition = pos;
-                }
-            }
-        }
+			debugText.text = boardText;
+		}
 
-        public void ChangeDrop(Vector2Int dropPosA, Vector2Int dropPosB)
-        {
-            if ((!ParameterManager.Instance.InBoard(dropPosA)) || (!ParameterManager.Instance.InBoard(dropPosB)))
-            {
-                Debug.LogWarning("画面外が指定されました。");
-                return;
-            }
-            Vector2 canvasPosA = ParameterManager.Instance.GetDropCanvasPosition(dropPosA);
-            Vector2 canvasPosB = ParameterManager.Instance.GetDropCanvasPosition(dropPosB);
+		public void ChangeDrop(Vector2Int dropPosA, Vector2Int dropPosB)
+		{
+			if ((!ParameterManager.Instance.InBoard(dropPosA)) || (!ParameterManager.Instance.InBoard(dropPosB)))
+			{
+				Debug.LogWarning("画面外が指定されました。");
+				return;
+			}
 
-            dropList[dropPosA.x][dropPosA.y].Move(canvasPosB);
-            dropList[dropPosB.x][dropPosB.y].Move(canvasPosA);
-            Drop temp = dropList[dropPosA.x][dropPosA.y];
-            dropList[dropPosA.x][dropPosA.y] = dropList[dropPosB.x][dropPosB.y];
-            dropList[dropPosB.x][dropPosB.y] = temp;
-        }
+			Vector2 canvasPosA = ParameterManager.Instance.GetDropCanvasPosition(dropPosA);
+			Vector2 canvasPosB = ParameterManager.Instance.GetDropCanvasPosition(dropPosB);
 
-        public async UniTaskVoid PuzzleProcess()
-        {
-            for(int i = 0; i< ParameterManager.Instance.boardSize.x;i++)
-            {
-                for(int j=0; j < ParameterManager.Instance.boardSize.y; j++)
-                {
-                    if (this.SearchCombo(new Vector2Int(i, j)))
-                    {
-                        DeleteDrop();
-                        await UniTask.Delay(TimeSpan.FromSeconds(0.5));
-                    }
-                }
-            }
-            
+			dropList[dropPosA.x][dropPosA.y].Move(canvasPosB);
+			dropList[dropPosB.x][dropPosB.y].Move(canvasPosA);
+			(dropList[dropPosA.x][dropPosA.y], dropList[dropPosB.x][dropPosB.y]) = (dropList[dropPosB.x][dropPosB.y], dropList[dropPosA.x][dropPosA.y]);
+		}
 
-        }
+		public async UniTaskVoid PuzzleProcess()
+		{
+			for (int i = 0; i < ParameterManager.Instance.boardSize.x; i++)
+			{
+				for (int j = 0; j < ParameterManager.Instance.boardSize.y; j++)
+				{
+					if (this.SearchCombo(new Vector2Int(i, j)))
+					{
+						DeleteDrop();
+						await UniTask.Delay(TimeSpan.FromSeconds(0.5));
+					}
+				}
+			}
+		}
 
-        public void Otosu()
-        {
-            FallDrop();
-            
-        }
+		public void Otosu()
+		{
+			FallDrop();
+		}
 
-        public void OtosuMitame()
-        {
-            for (int i = 0; i < ParameterManager.Instance.boardSize.x; i++)
-            {
-                for (int j = 0; j < ParameterManager.Instance.boardSize.y; j++)
-                {
-                    if (dropList[i][j] != null)
-                    {
-                        dropList[i][j].IndexMove(new Vector2Int(i,j));
-                    }
-                }
-            }
-        }
+		public void OtosuMitame()
+		{
+			for (int i = 0; i < ParameterManager.Instance.boardSize.x; i++)
+			{
+				for (int j = 0; j < ParameterManager.Instance.boardSize.y; j++)
+				{
+					if (dropList[i][j] != null)
+					{
+						dropList[i][j].IndexMove(new Vector2Int(i, j));
+					}
+				}
+			}
+		}
 
-        public bool SearchCombo(Vector2Int pos)
-        {
-            if (dropList[pos.x][pos.y].deleteFlag)
-            {
-                return false;
-            }
-           // Type  type = dropList[pos.x][pos.y].type;
-           
-            bool rightFlag =GetComboDirection(pos, Vector2Int.right);
-            bool upFlag =GetComboDirection(pos, Vector2Int.up);
-            return rightFlag || upFlag;
-        }
+		public bool SearchCombo(Vector2Int pos)
+		{
+			if (dropList[pos.x][pos.y].deleteFlag)
+			{
+				return false;
+			}
+			// Type  type = dropList[pos.x][pos.y].type;
 
-        public bool GetComboDirection(Vector2Int pos, Vector2Int searchVec)
-        {
-            Type type = dropList[pos.x][pos.y].type;
-            List<Drop> returnList=new List<Drop>();
-            returnList.Add(dropList[pos.x][pos.y]);
-            while (true)
-            {
-                pos += searchVec;
-                if (ParameterManager.Instance.InBoard(pos) && type == dropList[pos.x][pos.y].type)
-                {
-                    returnList.Add( dropList[pos.x][pos.y]);
-                }
-                else
-                {
-                    break;
-                }
-            }
+			bool rightFlag = GetComboDirection(pos, Vector2Int.right);
+			bool upFlag = GetComboDirection(pos, Vector2Int.up);
+			bool downFlag = GetComboDirection(pos, Vector2Int.down);
+			bool leftFlag = GetComboDirection(pos, Vector2Int.left);
+			return rightFlag || upFlag || downFlag || leftFlag;
+		}
 
+		public bool GetComboDirection(Vector2Int pos, Vector2Int searchVec)
+		{
+			Type type = dropList[pos.x][pos.y].type;
+			List<Drop> returnList = new List<Drop>();
+			returnList.Add(dropList[pos.x][pos.y]);
+			while (true)
+			{
+				pos += searchVec;
+				if (ParameterManager.Instance.InBoard(pos) && type == dropList[pos.x][pos.y].type)
+				{
+					SearchCombo(pos);
+					returnList.Add(dropList[pos.x][pos.y]);
+				}
+				else
+				{
+					break;
+				}
+			}
 
+			returnList.ForEach(drop => drop.deleteFlag = true);
 
-            return returnList.Count >= ParameterManager.Instance.destroyDropCount;
+			return returnList.Count >= ParameterManager.Instance.destroyDropCount;
+		}
 
-        }
-        private void DeleteDrop()
-        {
-            for (int i = 0; i < ParameterManager.Instance.boardSize.x;i++)
-            {
-                for (int j = 0; j < ParameterManager.Instance.boardSize.y; j++)
-                {
-                    if (dropList[i][j].deleteFlag)
-                    {
-                        dropList[i][j].Delete();     
-                    }
-                }
-            }
+		private void DeleteDrop()
+		{
+			for (int i = 0; i < ParameterManager.Instance.boardSize.x; i++)
+			{
+				for (int j = 0; j < ParameterManager.Instance.boardSize.y; j++)
+				{
+					if (dropList[i][j].deleteFlag)
+					{
+						dropList[i][j].Delete();
+					}
+				}
+			}
+		}
 
-            int a = 1;
-        }
 //TODO:ドロップの落ちる処理を実装する
-        private void FallDrop()
-        {
-            for (int i = 0; i < ParameterManager.Instance.boardSize.x; i++)
-            {
-                for (int j = 1; j < ParameterManager.Instance.boardSize.y; j++)
-                {
-                    if (dropList[i][j] == null)
-                    {
-                        continue;
-                    }
+		private void FallDrop()
+		{
+			for (int i = 0; i < ParameterManager.Instance.boardSize.x; i++)
+			{
+				for (int j = 1; j < ParameterManager.Instance.boardSize.y; j++)
+				{
+					if (dropList[i][j] == null)
+					{
+						continue;
+					}
 
-                    int searchPosY=j-1;
-                    while (dropList[i][searchPosY] == null)
-                    {
-                        searchPosY--;
-                        if (!ParameterManager.Instance.InBoard(new Vector2Int(i, searchPosY-1 )))
-                        {
-                            break;
-                        }
+					int searchPosY = j - 1;
+					while (dropList[i][searchPosY] == null)
+					{
+						searchPosY--;
+						if (!ParameterManager.Instance.InBoard(new Vector2Int(i, searchPosY - 1)))
+						{
+							break;
+						}
+					}
 
-                        
-
-                    }
-
-                    searchPosY++;
-                    
-
-                    if (j!=searchPosY )
-                    {
-                        try
-                        {
-                            dropList[i][searchPosY] = dropList[i][j];
-                            dropList[i][j] = null;
-                        }
-                        catch (System.Exception e)
-                        {
-                            Debug.LogError(e);
-                        }
-                    }
-                }
-            }
-        }
-
-        
-    }
+					searchPosY++;
+					
+					if (j != searchPosY)
+					{
+						try
+						{
+							dropList[i][searchPosY] = dropList[i][j];
+							dropList[i][j] = null;
+						}
+						catch (Exception e)
+						{
+							Debug.LogError(e);
+						}
+					}
+				}
+			}
+		}
+	}
 }
--- Puzzlegame/Assets/Mygamefolder/Scripts/Puzzle/Drop.cs
+++ Puzzlegame/Assets/Mygamefolder/Scripts/Puzzle/Drop.cs
@@ -10,62 +10,63 @@ using DG.Tweening;
 
 namespace Puzzle
 {
-    public class Drop
-    {
-        public Vector2Int pos;
-        public Type type;
-        public GameObject dropObject;
-        private Image image;
-        public bool deleteFlag ;
-        public Drop(Vector2Int pos, Type type,GameObject canvasObject)
-        {
-            this.deleteFlag = false;
-            this.pos=pos;
-            this.type=type;
-            this.dropObject = PoolingManager.Instance.PopPoolingObject(type);
-            if (this.dropObject)
-            {
-                this.dropObject.transform.parent = canvasObject.transform;
-                image = dropObject.GetComponent<Image>();
-                image.color = ColorManager.Instance.GetColor(this.type);
-                dropObject.GetComponent<RectTransform>(). sizeDelta= Vector3.one * ParameterManager.Instance.dropLenght;
-                dropObject.transform.localPosition = ParameterManager.Instance.GetDropCanvasPosition(pos);
-            }
-        }
+	public class Drop
+	{
+		public Vector2Int pos;
+		public Type type;
+		public GameObject dropObject;
+		private Image image;
+		public bool deleteFlag;
 
-        public void IndexMove(Vector2Int indexPos)
-        {
-            Move(ParameterManager.Instance.GetDropCanvasPosition(indexPos));
-        }
-        public void Move(Vector2 pos)
-        {
-            dropObject.transform.DOLocalMove(pos, ParameterManager.Instance.moveTime).SetEase(Ease.Linear);
-        }
-        public void ChangeColor(Color color)
-        {
-            image.color = color;
-            
-        }
+		public Drop(Vector2Int pos, Type type, GameObject canvasObject)
+		{
+			this.deleteFlag = false;
+			this.pos = pos;
+			this.type = type;
+			this.dropObject = PoolingManager.Instance.PopPoolingObject(type);
+			if (this.dropObject)
+			{
+				this.dropObject.transform.SetParent(canvasObject.transform, true);
+				image = dropObject.GetComponent<Image>();
+				image.color = ColorManager.Instance.GetColor(this.type);
+				dropObject.GetComponent<RectTransform>().sizeDelta = Vector3.one * ParameterManager.Instance.dropLenght;
+				dropObject.transform.localPosition = ParameterManager.Instance.GetDropCanvasPosition(pos);
+			}
+		}
 
-        public void Delete()
-        {
-            this.dropObject.SetActive(false);
-            PoolingManager.Instance.GetPoolingObjectPutBack(this.type,this.dropObject);
-        }
+		public void IndexMove(Vector2Int indexPos)
+		{
+			Move(ParameterManager.Instance.GetDropCanvasPosition(indexPos));
+		}
 
-        public void ViewPos()
-        {
-            
-        }
-[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-        static void EntryPoolingObject()
-        {
-            Enum.GetValues(typeof(Puzzle.Type)).Cast<Puzzle.Type>().ToList().ForEach(type =>
-            {
-                GameObject prefab = ParameterManager.Instance.dropPrefab;
-                PoolingManager.Instance.AddPoolingObject(type,prefab,30);
-            });
-        }
-        
-    }
+		public void Move(Vector2 pos)
+		{
+			dropObject.transform.DOLocalMove(pos, ParameterManager.Instance.moveTime).SetEase(Ease.Linear);
+		}
+
+		public void ChangeColor(Color color)
+		{
+			image.color = color;
+		}
+
+		public void Delete()
+		{
+			this.dropObject.SetActive(false);
+			PoolingManager.Instance.GetPoolingObjectPutBack(this.type, this.dropObject);
+		}
+
+		public void ViewPos()
+		{
+		}
+
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+		static void EntryPoolingObject()
+		{
+			Enum.GetValues(typeof(Puzzle.Type)).Cast<Puzzle.Type>().ToList().ForEach(type =>
+			{
+				GameObject prefab = ParameterManager.Instance.dropPrefab;
+				PoolingManager.Instance.AddPoolingObject(type, prefab, 30);
+			});
+		}
+	}
 }
